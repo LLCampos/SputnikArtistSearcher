c899a76210f96f2d397a1321b985e2330fbb2860
LLCampos <luis1212@sapo.pt>
2016-06-04 20:14:48 +0100

    consider two possible page layouts

--- SputnikArtistSearcher/src/page/SputnikAlbumPage.java
+++ SputnikArtistSearcher/src/page/SputnikAlbumPage.java
@@ -9,14 +9,24 @@ public class SputnikAlbumPage extends Page {
 	}
 
 	public SputnikArtistPage getArtistPage() {
+		
 		Element element = getPage_body().select("table > tbody > tr:eq(1) > td > table:eq(0) > tbody > tr > td > table > tbody > tr:eq(3) > td > div > div:eq(0) > div:eq(0) > h1 > a").first();
+		
+		if (element == null) {
+			element = getPage_body().select("table > tbody > tr:eq(1) > td > table:eq(0) > tbody > tr > td > table > tbody > tr:eq(3) > td > table > tbody > tr > td > table:eq(0) > tbody > tr > td:eq(1) > a").first();
+		}
+	    
 		String path = element.attr("href");
-		// teste
-		return new SputnikArtistPage(getPage_url().getProtocol() + "://" + getPage_url().getHost() + path);
+		return new SputnikArtistPage(getPage_url().getProtocol() + "://" + getPage_url().getHost() + "/" + path);
 	}
 	
 	public String getAlbumName() {
 		Element element = getPage_body().select("table > tbody > tr:eq(1) > td > table:eq(0) > tbody > tr > td > table > tbody > tr:eq(3) > td > div > div:eq(0) > div:eq(0) > h1 > span").first();
+		
+		if (element == null) {
+			element = getPage_body().select("table > tbody > tr:eq(1) > td > table:eq(0) > tbody > tr > td > table > tbody > tr:eq(3) > td > table > tbody > tr > td > table:eq(0) > tbody > tr > td:eq(1) > font").first();
+		}
+		
 		return element.html();
 	}
 }
