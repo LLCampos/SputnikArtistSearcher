d2d850b85abd10444b34cdb509d9104a14b2dd4f
LLCampos <luis1212@sapo.pt>
2016-06-04 11:33:38 +0100

    add tests

--- SputnikArtistSearcher/src/program/Tests.java
+++ SputnikArtistSearcher/src/program/Tests.java
@@ -1,5 +1,16 @@
 package program;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import data.ArtistToTry;
+import data.ArtistsToTry;
+import data.UnwantedArtists;
 import page.SputnikAlbumPage;
 import page.SputnikArtistPage;
 import page.SputnikUserPage;
@@ -8,7 +19,8 @@ public class Tests {
 
 	public static void main(String[] args) {
 		// Test 1
-		 /*
+		
+		
 		SputnikArtistPage artist_page = new SputnikArtistPage("http://www.sputnikmusic.com/bands/Arctic-Monkeys/1847/");
 		
 		if (artist_page.getArtistName().equals("Arctic Monkeys")) {
@@ -49,12 +61,37 @@ public class Tests {
 			System.out.println(album_page.getAlbumName());
 		};
 		
-		*/
+		// Test 3
+		try {
+			ArtistToTry artist = new ArtistToTry("The paper chase", "now you are one of us", "rock", new URL("http://www.sputnikmusic.com/bands/The-Paper-Chase/11287/"));
+			
+			if (artist.getYoutube_url().toString().equals("https://www.youtube.com/results?search_query=the+paper+chase+now+you+are+one+of+us")) {
+				System.out.println("Test 3 passed.");
+			} else {
+				System.out.println("https://www.youtube.com/results?search_query=the+paper+chase+now+you+are+one+of+us");
+				System.out.println(artist.getYoutube_url().toString());
+			}
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
 		
-		SputnikUserPage user_page = new SputnikUserPage("http://www.sputnikmusic.com/uservote.php?memberid=1043886");
-	    for (SputnikAlbumPage album_page : user_page.getFavoriteAlbumsPages()) {
-	    	System.out.println(album_page.getPage_url().toString());
-	    };
+		
+		// Test 4
+		
+		try {
+			UnwantedArtists unwanted_artists = new UnwantedArtists("program_files/UnwantedArtists.txt");
+			System.out.println(unwanted_artists.contains("Arcade Fire") ? "Test 4.1 Passed." : "Test 4.1 Failed.");
+			System.out.println(unwanted_artists.contains("afasdfsda") ? "Test 4.2 Failed." : "Test 4.2 Passed.");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+		//  SputnikUserPage user_page = new SputnikUserPage("http://www.sputnikmusic.com/uservote.php?memberid=1043886");
+		// ArrayList<SputnikAlbumPage> favorite_albums_pages = user_page.getFavoriteAlbumsPages();
+
 	}
 
 }
